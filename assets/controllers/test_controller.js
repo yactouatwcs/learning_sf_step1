3d7dca97e8fd466cd5614e45a9b226e0b436cc20
yactouat <yactouat@hotmail.com>
2022-11-13 12:42:31 +0100

    hooking up an event listener with Stimulus

--- assets/controllers/test_controller.js
+++ assets/controllers/test_controller.js
@@ -10,7 +10,14 @@ import { Controller } from '@hotwired/stimulus';
  * Delete this file or adapt it for your use!
  */
 export default class extends Controller {
-    connect() {
-        this.element.textContent = 'test Stimulus! Edit me in assets/controllers/test_controller.js';
+
+    // connect() {
+    //     console.log("test controller loaded");
+    // }
+
+    async doSomethingAsync(e) {
+        e.preventDefault();
+        console.log("doing something async");
     }
+
 }
